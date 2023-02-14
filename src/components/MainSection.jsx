9bd21355860f5a21bd1d55f1332b5602547615e7
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-14 08:38:24 +0100

    ...

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,9 +5,9 @@ import google from "../assets/google.png";
 const MainSection = () => {
   return (
     <div className="mt-28 flex flex-col lg:flex-row px-10 md:flex-row md:text-2xl lg:px-11 md:px-8 w-full">
-      <div className="lg:w-3/5 w-full">
+      <div className="lg:w-4/6 w-full">
         <h1
-          className="font-poppins font-semibold md:text-5xl lg:text-5xl text-4xl"
+          className="font-poppins font-semibold md:text-5xl lg:text-5xl text-5xl "
           style={{
             lineHeight: "70px",
           }}
@@ -17,30 +17,30 @@ const MainSection = () => {
           <b className="text-[#36BEB0]">Secured</b> <br /> For{" "}
           <b className="text-[#36BEB0]">You</b>.
         </h1>
-        <p className="pt-5 leading-loose font-poppins font-medium md:text-base text-xl">
+        <p className="pt-5 leading-loose font-poppins font-medium md:text-base text-lg">
           With <b>NOUNITE</b>, your Open and Distant learning experience will
           <br />
           never be the same! Automate so many processes <br /> and get school
           work done on the go!
         </p>
         <div className="flex items-center gap-5 mt-5 ">
-          <div className="bg-[#0F0E0E] text-[#FFFFFF] flex items-center gap-3 px-5 py-1 md:px-2 rounded-md cursor-pointer">
+          <div className="bg-[#0F0E0E] text-[#FFFFFF] flex items-center gap-3 px-2 lg:px-5 py-1 md:px-2 rounded-md cursor-pointer">
             <div>
               <img src={apple} />
             </div>
             <div className="flex flex-col ">
-              <p className="text-xs">Download on the</p>
-              <p className="md:text-base">App Store</p>
+              <p className="text-xs ">Download on the</p>
+              <p className="md:text-base text-sm">App Store</p>
             </div>
           </div>
-          <div className="bg-[#0F0E0E] text-[#FFFFFF] flex items-center gap-3 px-5 py-1 md:px-2 rounded-md cursor-pointer">
+          <div className="bg-[#0F0E0E] text-[#FFFFFF] flex items-center gap-3 px-2 lg:px-5 py-1 md:px-2 rounded-md cursor-pointer">
             <div>
               <img src={google} />
             </div>
 
             <div>
               <p className="text-xs">Get it on</p>
-              <p className="md:text-base">Google Play</p>
+              <p className="md:text-base text-sm">Google Play</p>
             </div>
           </div>
         </div>
