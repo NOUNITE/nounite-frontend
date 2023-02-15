e46b36139e08a0a080d680c196d55d1541d31637
Praisecodes <cmancoder@gmail.com>
2023-02-15 12:50:32 +0100

    Small fix on styling

--- src/components/newsletter/newLetter.jsx
+++ src/components/newsletter/newLetter.jsx
@@ -9,11 +9,11 @@ const NewsLetter = () => {
         <div className="flex overflow-hidden flex-row rounded-sm w-[70%] lg:w-[60%] bg-slate-100 justify-between px-1 py-1 items-center">
           <input 
             type="text"
-            className="h-[2rem] md:h-[3rem] font-poppins outline-none w-[55%] md:w-[60%] lg:w-[70%] text-[12px] bg-slate-100 px-1 md:px-3"
+            className="h-[2rem] md:h-[3rem] font-poppins outline-none w-[55%] md:w-[60%] lg:w-[70%] text-[12px] lg:text-base bg-slate-100 px-1 md:px-3"
             placeholder="Your email address"
           />
             <button
-              className="bg-btnColor text-[12px] lg:text-base font-poppins text-slate-100 placeholder:text-[#fff] py-2 px-3 rounded-sm"
+              className="bg-btnColor text-[12px] lg:text-base font-poppins text-slate-100 placeholder:text-[#fff] py-2 lg:px-4 px-1 rounded-sm"
               type="submit"
             >
               Subscribe
