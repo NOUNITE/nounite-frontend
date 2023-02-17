8280fdc2edc7116338206ef67fb3c4ca744d79f0
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-17 09:58:58 +0100

    i remove the routing to the faq,since it wont be a page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Faq from "./pages/faq/faq";
 import MainSection from "./pages/home/MainSection";
 import Waitlist from "./pages/waitlist/Waitlist";
 
 function App() {
   return (
     <>
-
       <Routes>
-
         <Route path="/" element={<MainSection />} />
         <Route path="/waitlist" element={<Waitlist />} />
-        <Route path="/faq" element={<Faq />} />
-
       </Routes>
     </>
-
   );
 }
 
