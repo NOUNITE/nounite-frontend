507779198ed2b4a840aaef5a4d2f47bc5ed64013
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-17 13:43:33 +0100

    change the text in the paragrapg and adjust some code

--- src/pages/home/AboutUs.jsx
+++ src/pages/home/AboutUs.jsx
@@ -6,24 +6,27 @@ const AboutUs = () => {
       <div className="w-full pt-12">
         <img src={long} alt="faq img" className="w-full" />
       </div>
-      <div className="flex justify-between flex-col lg:flex-row md:flex-row  lg:px-11 md:px-8 py-16">
+      <div className="flex justify-between items-center flex-col lg:flex-row md:flex-row  lg:px-8 md:px-5 py-16">
         <div>
-          <h1 className="font-poppins xl:text-[54px] lg:text-[40px] text-[30px] text-[#0F0E0E] font-semibold border border-[#0D4459] border-solid rounded-full px-5">
+          <h1 className="font-poppins xl:text-[54px] lg:text-[40px] text-[30px] text-[#0F0E0E] font-semibold border border-[#0D4459] border-solid rounded-full w-fit px-5">
             What is NOUNITE?
           </h1>
-          <p className="font-poppins lg:text-[16px] md:text-[15px] xl:text-[18px] text-[15px] text-[#000000] font-normal mt-5">
-            NOUNITE is the solution to all your academic
-            <br /> struggles as a student of NOUN. NOUNITE brings
-            <br /> those stressful processes to your fingertips.
-            <br /> With NOUNITE, you get to experience seamless and <br /> very
-            easy payment of school fees, very fast and easy <br /> course and
-            examinations registration and fast and
-            <br /> easy access to your Tutor Marked Assignments (TMAs).
-            <br /> With NOUNITE, students of NOUN get to experience a<br />
-            smoother academic pursuit!
+          <p className="font-poppins lg:text-[16px] md:text-[14px] xl:text-[18px] text-[15px] text-[#000000] font-normal mt-5">
+            NOUNITE is a platform that offers a solution
+            <br /> to all your academic struggles as a student of NOUN.
+            <br />
+            NOUNITE brings those stressful processes to your fingertips.
+            <br /> With NOUNITE, you get to experience seamless and
+            <br /> very easy payment of school fees, very fast and easy
+            <br /> course and examinations registration and fast and easy
+            <br /> access to your Tutor Marked Assignments (TMAs).
+            <br /> With NOUNITE, students of NOUN get to experience a smoother{" "}
+            <br />
+            academic pursuit! We make Open Learning In Nigeria
+            <br /> Simpler than ever!
           </p>
         </div>
-        <div className="lg:w-[480px] xl-[450px] w-[450px]">
+        <div className="lg:w-[430px] xl-[500px] w-[450px] md:w-[360px] mt-16 lg:mt-0 md:mt-0 items-end">
           <img src={students} />
         </div>
       </div>
