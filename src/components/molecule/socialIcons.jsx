944978d13500515be0b2fddb0f179739ad9568e0
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-20 19:59:41 +0100

    add link url

--- src/components/molecule/socialIcons.jsx
+++ src/components/molecule/socialIcons.jsx
@@ -1,21 +1,20 @@
 import { facebook, twitter, linkedin } from "../../assets/index";
 
-
 export default function SocialIcons() {
-  return(
+  return (
     <div className=" flex  items-center gap-5 pt-3 lg:pt-0 md:pt-0">
       <div className="cursor-pointer border border-textHover border-solid rounded-[100%] py-3 px-4">
-        <a href="/" >
-          <img src={facebook}/>
+        <a href="https://facebook.com/nouniteapp" target="_blank">
+          <img src={facebook} />
         </a>
       </div>
       <div className="cursor-pointer border border-textHover border-solid rounded-[100%] py-3 px-3">
-        <a href="/">
+        <a href="https://twitter.com/nouniteapp" target="_blank">
           <img src={twitter} />
         </a>
       </div>
       <div className="cursor-pointer border border-textHover border-solid rounded-[100%] py-3 px-3">
-        <a href="/">
+        <a href="https://linkedin.com/company/nouniteapp" target="_blank">
           <img src={linkedin} />
         </a>
       </div>
