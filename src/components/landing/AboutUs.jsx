fb60c998ae8cefeeb4b4b929926f334ff1586745
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-20 18:52:01 +0100

    add padding to the main div and reduce the text for the sm

--- src/components/landing/AboutUs.jsx
+++ src/components/landing/AboutUs.jsx
@@ -6,9 +6,9 @@ const AboutUs = () => {
       <div className="w-full pt-12">
         <img src={long} alt="faq img" className="w-full" />
       </div>
-      <div className="flex justify-between px-10 flex-col lg:flex-row md:flex-row  lg:px-8 md:px-5 py-16">
+      <div className="flex justify-between px-10 sm:px-5 flex-col lg:flex-row md:flex-row  lg:px-8 md:px-5 py-16">
         <div>
-          <h1 className="font-poppins text-center lg:text-[40px] text-[30px] text-textHover font-semibold border border-btnColor border-solid rounded-[200%] py-2 px-8">
+          <h1 className="font-poppins text-center lg:text-[40px] text-[30px] sm:text-[20px] text-textHover font-semibold border border-btnColor border-solid rounded-[200%] py-2 px-8">
             What is NOUNITE?
           </h1>
           <p className="font-poppins lg:text-[16px] md:text-[14px] xl:text-[18px] text-[15px] text-black font-normal mt-5">
@@ -26,7 +26,7 @@ const AboutUs = () => {
             <br /> Simpler than ever!
           </p>
         </div>
-        <div className="items-center mt-10 lg:mt-0 w-[300px] lg:w-[450px] xl-[500px] w-[450px] md:w-[400px]">
+        <div className="items-center mt-10 lg:mt-0 sm:w-[300px] lg:w-[450px] xl-[500px] w-[450px] md:w-[400px]">
           <img src={students} />
         </div>
       </div>
