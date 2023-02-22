cc1bfdb4306bc60768cafb0366a2afac9b2afcff
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-22 11:19:02 +0100

    adjust h1,p and the image width

--- src/components/landing/AboutUs.jsx
+++ src/components/landing/AboutUs.jsx
@@ -8,10 +8,10 @@ const AboutUs = () => {
       </div>
       <div className="flex justify-between px-10 sm:px-5 flex-col lg:flex-row md:flex-row  lg:px-8 md:px-5 py-16">
         <div>
-          <h1 className="font-poppins text-center lg:text-[40px] text-[30px] sm:text-[20px] text-textHover font-semibold border border-btnColor border-solid rounded-[200%] py-2 px-8">
+          <h1 className="font-poppins text-center lg:text-[40px] md:text-[25px] text-[30px] sm:text-[20px] text-textHover font-semibold border border-btnColor border-solid rounded-[200%] py-2 px-8 w-[300px] lg:w-[500px]">
             What is NOUNITE?
           </h1>
-          <p className="font-poppins lg:text-[16px] md:text-[14px] xl:text-[18px] text-[15px] text-black font-normal mt-5">
+          <p className="font-poppins lg:text-[16px] md:text-[10px] xl:text-[18px] text-[15px] text-black font-normal mt-5">
             NOUNITE is a platform that offers a solution
             <br /> to all your academic struggles as a student of NOUN.
             <br />
@@ -26,7 +26,7 @@ const AboutUs = () => {
             <br /> Simpler than ever!
           </p>
         </div>
-        <div className="items-center mt-10 lg:mt-0 sm:w-[340px] lg:w-[450px] xl-[500px] w-[450px] md:w-[400px]">
+        <div className="items-center mt-10 md:mt-0 lg:mt-0 sm:w-[250px] xm:w-[280px] lg:w-[390px] xl-[500px] w-[280px] md:w-[280px] ml-[60px] sm:ml-[70px] xm:ml-[100px] md:ml-0 lg:ml-0">
           <img src={students} />
         </div>
       </div>
